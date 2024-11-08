534e710a09a33aaf9ce6058f50271d53e92db230
akarahdev <endisticbutitsforgit@gmail.com>
2024-11-08 16:53:08 -0500

    feat: add packet buffer

--- dragonet/src/lib.rs
+++ dragonet/src/lib.rs
@@ -1,2 +1,5 @@
+#![allow(unused)]
+
 mod protocol;
 mod server;
+mod buffer;
--- dragonet/src/protocol.rs
+++ dragonet/src/protocol.rs
@@ -1,5 +1,7 @@
+use crate::buffer::PacketBuf;
+
 pub trait Protocol {
-    fn encode(&self, packet: &Self) -> Vec<u8>;
-    fn decode(&self, bytes: Vec<u8>) -> Self;
+    fn encode(&self, packet: &Self) -> PacketBuf;
+    fn decode(&self, buf: PacketBuf) -> Self;
     fn size_of(packet: &Self) -> u32;
 }
